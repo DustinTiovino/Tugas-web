248533e6ffe80e25866a65645cd490d0fd7ffdef
HazelD11 <hazel.dixon@itbss.ac.id>
2023-03-16 20:08:24 +0700

    menambahkan fitur js

---
+++ javascript.js
@@ -0,0 +1,96 @@
+function fotocopyOpen() {
+    var fotocopy_buka = document.getElementById("fotocopy_buka").value;
+
+    if(fotocopy_buka == "fotocopy"){
+        document.getElementById("fotocopy_detail").style.display = "block";
+    }
+}
+
+function fotocopyClose() {
+    var fotocopy_tutup = document.getElementById("fotocopy_tutup").value;
+
+    if(fotocopy_tutup == "tutup"){
+        document.getElementById("fotocopy_detail").style.display = "none";
+    }
+}
+
+function cuciFotoOpen() {
+    var cuciFoto_buka = document.getElementById("cuciFoto_buka").value;
+
+    if(cuciFoto_buka == "Cuci Foto dan Pas Foto"){
+        document.getElementById("cuciFoto_detail").style.display = "block";
+    }
+}
+
+function cuciFotoClose() {
+    var cuciFoto_tutup = document.getElementById("cuciFoto_tutup").value;
+
+    if(cuciFoto_tutup == "tutup"){
+        document.getElementById("cuciFoto_detail").style.display = "none";
+    }
+}
+
+function editFotoOpen() {
+    var editFoto_buka = document.getElementById("editFoto_buka").value;
+
+    if(editFoto_buka == "Jasa Edit Foto"){
+        document.getElementById("editFoto_detail").style.display = "block";
+    }
+}
+
+function editFotoClose() {
+    var editFoto_tutup = document.getElementById("editFoto_tutup").value;
+
+    if(editFoto_tutup == "tutup"){
+        document.getElementById("editFoto_detail").style.display = "none";
+    }
+}
+
+function scanOpen() {
+    var scan_buka = document.getElementById("scan_buka").value;
+
+    if(scan_buka == "Scan"){
+        document.getElementById("scan_detail").style.display = "block";
+    }
+}
+
+function scanClose() {
+    var scan_tutup = document.getElementById("scan_tutup").value;
+
+    if(scan_tutup == "tutup"){
+        document.getElementById("scan_detail").style.display = "none";
+    }
+}
+
+function printOpen() {
+    var print_buka = document.getElementById("print_buka").value;
+
+    if(print_buka == "Print"){
+        document.getElementById("print_detail").style.display = "block";
+    }
+}
+
+function printClose() {
+    var print_tutup = document.getElementById("print_tutup").value;
+
+    if(print_tutup == "tutup"){
+        document.getElementById("print_detail").style.display = "none";
+    }
+}
+
+function laminatingOpen() {
+    var laminating_buka = document.getElementById("laminating_buka").value;
+
+    if(laminating_buka == "Laminating"){
+        document.getElementById("laminating_detail").style.display = "block";
+    }
+}
+
+function laminatingClose() {
+    var laminating_tutup = document.getElementById("laminating_tutup").value;
+
+    if(laminating_tutup == "tutup"){
+        document.getElementById("laminating_detail").style.display = "none";
+    }
+}
+
